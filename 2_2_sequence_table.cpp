29626716a1efc953a7f229a61ec46d155b069a86
zhuBoDian <379837872@qq.com>
2023-01-08 23:09:36 +0800

    初始

---
+++ 2_2_sequence_table.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MaxSize 10      //定义最大长度
+typedef struct {
+    int data[MaxSize];      //用静态的“数组"存放数据元素
+    int length;     //顺序表的当前长度
+}SqList;        //顺序表的类型定义
+
+//基本操作—初始化一个顺序表
+void InitList(SqList &L){
+    for (int i = 0; i < MaxSize; i++)
+        L.data[i] = 0;//将所有数据元素设置为默认初始值
+    
+    L.length = 0;//顺序表初始长度为0
+}
+
+int main() {
+    SqList L;    // 声明一个顺序表
+    InitList(L); // 初始化顺序表l l....未完待续，后续操作
+    return 0;
+
+}
---
+++ hello  world.cpp
@@ -0,0 +1,19 @@
+#include <iostream>//头文件
+
+using namespace std;
+
+int main()//主函数
+
+{
+    int a = 1;
+
+    a = a * a;
+    a = a * a;
+
+    cout << "Hello world!\n"; //cout是输出，\n是换行符，“”中可输入中文
+
+    return 0;//结束程序   
+    
+
+}
+
